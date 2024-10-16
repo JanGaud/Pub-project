156fb595274fd53c5aab0a0c0eb956f39a995e21
JanGaud <JanisGaudreault.jg@gmail.com>
2024-10-16 13:43:34 -0400

    added badged to food

--- src/lib/components/menu/Food.svelte
+++ src/lib/components/menu/Food.svelte
@@ -48,15 +48,15 @@
                 <p class={item.out_of_stock ? 'text-black/25' : ''}>{item.ingredients}</p>
               {/if}
               <!-- Display boolean attributes as images if true -->
-              <div class="flex gap-2 mt-2">
+              <div class="flex gap-3 mt-2">
                 {#if item.vegan}
-                  <img src="static/dietaryTag/vegan.png" alt="Vegan" class="w-8 h-8" />
+                  <img src="/dietaryTag/vegan.png" alt="Vegan" class="w-8 h-8" />
                 {/if}
                 {#if item.vegetarian}
-                  <img src="static/dietaryTag/vegetarian.png" alt="Vegetarian" class="w-8 h-8" />
+                  <img src="/dietaryTag/vegetarian.png" alt="Vegetarian" class="w-8 h-8" />
                 {/if}
                 {#if item.spicy}
-                  <img src="static/dietaryTag/red-chili-pepper.png" alt="Spicy" class="w-8 h-8" />
+                  <img src="/dietaryTag/red-chili-pepper.png" alt="Spicy" class="w-8 h-8" />
                 {/if}
               </div>
             </div>
@@ -92,15 +92,15 @@
                 <p class={item.out_of_stock ? 'text-black/25' : ''}>{item.ingredients}</p>
               {/if}
               <!-- Display boolean attributes as images if true -->
-              <div class="flex gap-2 mt-2">
+              <div class="flex gap-3 mt-2">
                 {#if item.vegan}
-                  <img src="static/dietaryTag/vegan.png" alt="Vegan" class="w-8 h-8" />
+                  <img src="/dietaryTag/vegan.png" alt="Vegan" class="w-8 h-8" />
                 {/if}
                 {#if item.vegetarian}
-                  <img src="static/dietaryTag/vegetarian.png" alt="Vegetarian" class="w-8 h-8" />
+                  <img src="/dietaryTag/vegetarian.png" alt="Vegetarian" class="w-8 h-8" />
                 {/if}
                 {#if item.spicy}
-                  <img src="static/dietaryTag/red-chili-pepper.png" alt="Spicy" class="w-8 h-8" />
+                  <img src="/dietaryTag/red-chili-pepper.png" alt="Spicy" class="w-8 h-8" />
                 {/if}
               </div>
             </div>
