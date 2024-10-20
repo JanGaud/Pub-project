aa07a3bb30b301f1f207076523894f0cd4d84493
JanGaud <JanisGaudreault.jg@gmail.com>
2024-10-20 12:11:27 -0400

    meta image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,7 +31,7 @@
 	{/if}
 	{#if $page.data.meta_image}
 		<meta name="og:image" content={$page.data.meta_image.url} />
-		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:card" content={$page.data.meta_image.url} />
 	{/if}
 </svelte:head>
 
